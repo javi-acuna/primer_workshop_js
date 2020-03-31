006b9e170674f4d0b58627960580b46740507e11
Javier <javi.emiliano@gmail.com>
2020-03-31 01:57:55 -0300

    agrego manejo de pedidos version inicial

--- primer_workshop.js
+++ primer_workshop.js
@@ -6,6 +6,12 @@ class usuarios{
         this.telefono = telefono; 
     }  
 
+    getNombre(){
+        return this.nombre; }
+
+    getApellido(){
+        return this.apellido; }
+
     listadoUsuarios(){
      return "Nombre: " + this.nombre +" Apellido: "+ this.apellido + "\nEmail: " + this.email + " Telefono: " +this.telefono;   }
 
@@ -20,9 +26,26 @@ class productos{
     listadoProductos(){
         return "Nombre: " + this.nombre +" Precio: "+ this.precio;   }
 }
-   
-arrayUsuario=[];
-arrayProducto=[];
+
+class pedidos{
+    constructor(usuario, producto){
+        this.usuario = usuario;
+        this.producto = producto;
+    }
+
+    getUsuario(){
+        return this.usuario; }
+
+    getProducto(){
+        return this.producto; }
+
+    listadoPedidos(){
+        return "Nombre: " + this.usuario +" Producto: "+ this.producto;   }
+}
+
+arrayPedidos = [];
+arrayUsuario = [];
+arrayProducto = [];
 
 do{
 opcion = parseInt(prompt("Ingrese opcion:\n1. Modulo usuario\n2. Modulo producto\n3. Modulo pedidos\n4. Salir del programa"));
@@ -32,15 +55,15 @@ switch (opcion) {
         op1 = parseInt(prompt("Ingrese opcion:\n1. Alta usuario\n2. Listado de usuarios\nPresione cualquier numero para regresar al menú inicial"));
         switch (op1) {
         case 1:
-                do{
-                nombre= prompt("Ingrese nombre: (Stop) para salir ");
-                if(nombre !="Stop"){
+            do{
+            nombre= prompt("Ingrese nombre: (Stop) para salir ");
+            if(nombre !="Stop"){
                 apellido = prompt("Ingrese apellido: ");
                 email = prompt("Ingrese email: ");
                 telefono = parseInt(prompt("Ingrese su telefono: "));
                 user = new usuarios(nombre, apellido, email, telefono);
                 arrayUsuario.push(user);}
-                }while(nombre!="Stop");
+            }while(nombre!="Stop");
             break;
 
         case 2:
@@ -60,15 +83,15 @@ switch (opcion) {
 
     case 2:
      do {
-        op2 = parseInt(prompt("Ingrese opcion:\n1. alta producto\n2. listado de producto\nPresione cualquier numero para regresar al menú inicial"));
+        op2 = parseInt(prompt("Ingrese opcion:\n1. Alta producto\n2. Listado de producto\nPresione cualquier numero para regresar al menú inicial"));
         switch (op2) {
         case 1:
             do{
             nombre = prompt("Ingrese nombre: (Stop) para salir ");
             if(nombre !="Stop"){
-            precio = prompt("Ingrese precio: ");
-            prod = new productos(nombre, precio );
-            arrayProducto.push(prod);}
+                precio = prompt("Ingrese precio: ");
+                prod = new productos(nombre, precio);
+                arrayProducto.push(prod);}
             }while(nombre!="Stop");
             break;
 
@@ -86,7 +109,56 @@ switch (opcion) {
         
      }while(op2==1 || op2==2);
 
+     continue;
+    
+     case 3:
+        do {
+            op3 = parseInt(prompt("Ingrese opcion:\n1. Alta pedido\n2. Listado de pedidos\nPresione cualquier numero para regresar al menú inicial"));
+            switch (op3) {
+            case 1:
+                do{
+                    do{
+                    let cadenaUser ='';
+                    for (i=0;i<arrayUsuario.length;i++){
+                        cadenaUser +='\n '+[i+1]+'.- '+ arrayUsuario[i].getNombre()+' '+ arrayUsuario[i].getApellido();
+                    }
+                    userPedido = prompt("Seleccione usuario que realiza el pedido: (Stop) para salir \n"+cadenaUser);
+                    if(userPedido=="Stop") break;
+                    }while(userPedido>arrayUsuario.length || userPedido==0);
+
+                    do{
+                    if(userPedido=="Stop") break;
+                    let cadenaProd ='';
+                    for (i=0;i<arrayProducto.length;i++){
+                        cadenaProd +='\n '+[i+1]+'.- '+ arrayProducto[i].listadoProductos();
+                    }
+                    prodPedido = prompt("Seleccione producto que realiza el pedido: \n"+cadenaProd);
+                    }while(userPedido>arrayProducto.length || userPedido==0);
+                    ped=new pedidos(userPedido, prodPedido);
+                    arrayPedidos.push(ped);
+                }while(userPedido!="Stop");
+                break;
+    
+            case 2:
+                let cadenaPed ='';
+                for (i=0; i<arrayPedidos.length; i++){
+                    cadenaPed +='\n '+[i+1]+'.- '+ arrayPedidos[i].listadoPedidos();
+                }
+                alert(cadenaPed);
+                break;
+    
+            default:
+                break;
+            }
+            
+         }while(op3==1 || op3==2);
+         
+     continue;
+
     case 4: 
+        break;
+        
+    default:
         break;
 };
 
